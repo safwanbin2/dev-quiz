3f9d3b4d960720eb054a6190202f2cb71ef11357
Safwan <safwanridwan321@gmail.com>
2022-10-11 14:58:00 +0600

    added taost

--- src/components/QuizItem/QuizItem.js
+++ src/components/QuizItem/QuizItem.js
@@ -1,19 +1,41 @@
 import React from 'react';
 import './QuizItem.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye } from '@fortawesome/free-solid-svg-icons'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const QuizItem = ({quiz}) => {
-    console.log(quiz)
-    const {correctAnswer, id, options, question} = quiz;
+const QuizItem = ({ quiz }) => {
+    const { correctAnswer, options, question } = quiz;
+    const handleshowBtn = () => {
+        toast.success(`The answer is ---${correctAnswer}`, {autoClose: 2000})
+    }
+    const handleSelect = e => {
+        if(e === correctAnswer){
+            toast.success('The Answer is Right', {autoClose: 500})
+        }
+        else{
+            toast.error('The Answer is Wrong', {autoClose: 500})
+        }
+    }
     return (
-        <div className='p-8 border-2 border-accent shadow-lg my-8'>
+        <div className='p-8 border-2 border-accent rounded-md shadow-lg my-8'>
+            <div onClick={handleshowBtn} className='text-end'>
+                <FontAwesomeIcon icon={faEye}></FontAwesomeIcon>
+            </div>
             <h1 className='text-2xl font-semibold'>{question}</h1>
             <div className='my-4 font-semibold ml-8'>
-                {options[1] && <h4 className='p-2 rounded cursor-pointer quiz-item'>1. {options[1]}</h4>}
-                {options[0] && <h4 className='p-2 rounded cursor-pointer quiz-item'>2. {options[0]}</h4>}
-                {options[2] && <h4 className='p-2 rounded cursor-pointer quiz-item'>3. {options[2]}</h4>}
-                {options[3] && <h4 className='p-2 rounded cursor-pointer quiz-item'>4. {options[3]}</h4>}
-                {options[4] && <h4 className='p-2 rounded cursor-pointer quiz-item'>5. {options[4]}</h4>}
+                {options[0] && <div className='flex items-center'>1. <h4 onClick={() => handleSelect(options[0])} className='p-2 rounded cursor-pointer quiz-item'>{options[0]}</h4></div>}
+
+                {options[1] && <div className='flex items-center'>2. <h4 onClick={() => handleSelect(options[1])} className='p-2 rounded cursor-pointer quiz-item'>{options[1]}</h4></div>}
+
+                {options[2] && <div className='flex items-center'>3. <h4 onClick={() => handleSelect(options[2])} className='p-2 rounded cursor-pointer quiz-item'>{options[2]}</h4></div>}
+
+                {options[3] && <div className='flex items-center'>4. <h4 onClick={() => handleSelect(options[3])} className='p-2 rounded cursor-pointer quiz-item'>{options[3]}</h4></div>}
+
+                {options[4] && <div className='flex items-center'>5. <h4 onClick={() => handleSelect(options[4])} className='p-2 rounded cursor-pointer quiz-item'>{options[4]}</h4></div>}
             </div>
+            <ToastContainer></ToastContainer>
         </div>
     );
 };
